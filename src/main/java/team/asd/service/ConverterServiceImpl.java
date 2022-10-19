a1d30abef5bf7e3d31d3d996726ceb3bf1d6aa79
Tishchenko666 <s5187565@gmail.com>
2022-10-19 18:17:35 +0200

    Configure Code Style Scheme for IDEA.

--- src/main/java/team/asd/service/ConverterServiceImpl.java
+++ src/main/java/team/asd/service/ConverterServiceImpl.java
@@ -5,23 +5,23 @@ import java.time.format.DateTimeParseException;
 
 public class ConverterServiceImpl implements IsConverterService {
 
-    @Override
-    public String convertIntegerIntoString(Integer value) {
-        return value == null ? null : value.toString();
-    }
+	@Override
+	public String convertIntegerIntoString(Integer value) {
+		return value == null ? null : value.toString();
+	}
 
-    @Override
-    public Integer convertStringIntoInteger(String value) {
-        return value == null ? null : Integer.valueOf(value);
-    }
+	@Override
+	public Integer convertStringIntoInteger(String value) {
+		return value == null ? null : Integer.valueOf(value);
+	}
 
-    @Override
-    public Double convertStringIntoDouble(String value) {
-        return value == null ? null : Double.valueOf(value);
-    }
+	@Override
+	public Double convertStringIntoDouble(String value) {
+		return value == null ? null : Double.valueOf(value);
+	}
 
-    @Override
-    public LocalDate convertStringIntoLocalDate(String dateString) throws DateTimeParseException {
-        return dateString == null ? null : LocalDate.parse(dateString);
-    }
+	@Override
+	public LocalDate convertStringIntoLocalDate(String dateString) throws DateTimeParseException {
+		return dateString == null ? null : LocalDate.parse(dateString);
+	}
 }
