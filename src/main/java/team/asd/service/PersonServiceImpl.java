9e130d47f42a68db1a56dba30541ef76392c230d
Tishchenko666 <s5187565@gmail.com>
2022-10-20 21:57:19 +0200

    PersonServiceImpl was refactored. If clauses with single line get {}. Checking null and empty clauses in method collectPersonsWithNameStartsWith() was changed on CollectionUtils.isEmpty() and new list creation - on Collections.emptyList(). In method collectPersonsByAge() new map creation was changed on Collections.emptyMap().

--- src/main/java/team/asd/service/PersonServiceImpl.java
+++ src/main/java/team/asd/service/PersonServiceImpl.java
@@ -1,11 +1,11 @@
 package team.asd.service;
 
 import lombok.NonNull;
+import org.apache.commons.collections4.CollectionUtils;
 import team.asd.entities.IsPerson;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
@@ -16,10 +16,12 @@ public class PersonServiceImpl implements IsPersonService {
 	@Override
 	@NonNull
 	public List<IsPerson> collectPersonsWithNameStartsWith(List<IsPerson> personList, String prefix) {
-		if (personList == null || personList.isEmpty())
-			return new ArrayList<>();
-		if (prefix == null || prefix.equals(""))
+		if (CollectionUtils.isEmpty(personList)) {
+			return Collections.emptyList();
+		}
+		if (prefix == null || prefix.equals("")) {
 			return personList;
+		}
 		return personList.stream()
 				.filter(p -> p != null && p.getName() != null && p.getName()
 						.startsWith(prefix))
@@ -29,8 +31,9 @@ public class PersonServiceImpl implements IsPersonService {
 	@Override
 	@NonNull
 	public Map<Integer, List<IsPerson>> collectPersonsByAge(List<IsPerson> personList) {
-		if (personList == null)
-			return new HashMap<>();
+		if (personList == null) {
+			return Collections.emptyMap();
+		}
 		return personList.stream()
 				.filter(p -> p != null && p.getAge() != null && p.getAge() >= 0)
 				.sorted(Comparator.comparing(IsPerson::getAge))
@@ -40,13 +43,15 @@ public class PersonServiceImpl implements IsPersonService {
 	@Override
 	@NonNull
 	public Double calculateAverageAge(List<IsPerson> personList) {
-		if (personList == null)
+		if (personList == null) {
 			return 0D;
+		}
 		personList = personList.stream()
 				.filter(p -> p != null && p.getAge() != null && p.getAge() >= 0)
 				.collect(Collectors.toList());
-		if (personList.isEmpty())
+		if (personList.isEmpty()) {
 			return 0D;
+		}
 		return personList.stream()
 				.mapToInt(IsPerson::getAge)
 				.summaryStatistics()
@@ -56,8 +61,9 @@ public class PersonServiceImpl implements IsPersonService {
 	@Override
 	@NonNull
 	public IntSummaryStatistics sumAndCountAge(List<IsPerson> personList) {
-		if (personList == null)
+		if (personList == null) {
 			return new IntSummaryStatistics();
+		}
 		return personList.stream()
 				.filter(p -> p != null && p.getAge() != null && p.getAge() >= 0)
 				.mapToInt(IsPerson::getAge)
