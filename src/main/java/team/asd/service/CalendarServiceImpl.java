0dee164478b34b1b47015e5d5f343926dce5af1d
Tishchenko666 <s5187565@gmail.com>
2022-10-26 21:13:23 +0200

    In method defineCountInRange() -> convert LocalDate to LocalDateTime. In method reformatToLocalDate() -> use DateTimeFormatter, split string, check with patterns and ranges, join array to string, parse LocalDate.

--- src/main/java/team/asd/service/CalendarServiceImpl.java
+++ src/main/java/team/asd/service/CalendarServiceImpl.java
@@ -6,10 +6,14 @@ import team.asd.exceptions.WrongArgumentException;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 
 public class CalendarServiceImpl implements IsCalendarService {
 
@@ -42,8 +46,9 @@ public class CalendarServiceImpl implements IsCalendarService {
 		if (fromDate == null || toDate == null || unit == null) {
 			throw new WrongArgumentException("Wrong argument");
 		}
-		//!! seconds -> local date time
-		return unit.between(fromDate, toDate);
+		LocalDateTime from = LocalDateTime.of(fromDate, LocalTime.MIN);
+		LocalDateTime to = LocalDateTime.of(toDate, LocalTime.MIN);
+		return unit.between(from, to);
 	}
 
 	@Override
@@ -71,7 +76,31 @@ public class CalendarServiceImpl implements IsCalendarService {
 		if (StringUtils.isEmpty(dateString)) {
 			throw new DateTimeException("Wrong date");
 		}
-		return null;
+		String[] dateParts = dateString.split(" ");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dMMMyyyy");
+		//0
+		dateParts[0] = dateParts[0].matches("\\d+[A-Za-z]+") ? dateParts[0].replace("[A-Za-z]+", "") : null;
+		int day;
+		if (dateParts[0] != null) {
+			day = Integer.parseInt(dateParts[0]);
+			if (day < 1 || day > 31) {
+				throw new DateTimeException("Wrong date");
+			}
+		}
+		//1
+		if (Arrays.stream(Month.values())
+				.noneMatch(month -> month.toString()
+						.substring(0, 3)
+						.equalsIgnoreCase(dateParts[1]))) {
+			throw new DateTimeException("Wrong date");
+		}
+		//2
+		int year = dateParts[2].matches("\\d+") ? Integer.parseInt(dateParts[2]) : -1;
+		if (year < 1000 || year > 3000) {
+			throw new DateTimeException("Wrong date");
+		}
+		dateString = String.join("", dateParts);
+		return LocalDate.parse(dateString, formatter);
 	}
 
 }
