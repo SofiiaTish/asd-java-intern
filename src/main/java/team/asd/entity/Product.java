171b6ce4c7d172c876ab378e0a573ba7c4948aac
Sofiia Tishchenko <s.tischenko01@gmail.com>
2022-11-14 11:13:49 +0100

    #37 use formatter to Product

--- src/main/java/team/asd/entity/Product.java
+++ src/main/java/team/asd/entity/Product.java
@@ -13,42 +13,42 @@ import java.time.LocalDateTime;
 @Builder
 public class Product {
 
-    private Integer id;
-    private Integer supplierId; //relation to party record
-    private String name;
-    private ProductState state;
-    private String currency;
-    private Integer guestsNumber;
-    private Double longitude;
-    private Double latitude;
-    private String physicalAddress;
-    private LocalDateTime version;
+	private Integer id;
+	private Integer supplierId; //relation to party record
+	private String name;
+	private ProductState state;
+	private String currency;
+	private Integer guestsNumber;
+	private Double longitude;
+	private Double latitude;
+	private String physicalAddress;
+	private LocalDateTime version;
 
-    public Product() {
-    }
+	public Product() {
+	}
 
-    public Product(Integer supplierId, String name, ProductState state, String currency, Integer guestsNumber, Double longitude, Double latitude, String physicalAddress) {
-        this.supplierId = supplierId;
-        this.name = name;
-        this.state = state;
-        this.currency = currency;
-        this.guestsNumber = guestsNumber;
-        this.longitude = longitude;
-        this.latitude = latitude;
-        this.physicalAddress = physicalAddress;
-    }
+	public Product(Integer supplierId, String name, ProductState state, String currency, Integer guestsNumber, Double longitude, Double latitude, String physicalAddress) {
+		this.supplierId = supplierId;
+		this.name = name;
+		this.state = state;
+		this.currency = currency;
+		this.guestsNumber = guestsNumber;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.physicalAddress = physicalAddress;
+	}
 
-    public Product(Integer id, Integer supplierId, String name, ProductState state, String currency, Integer guestsNumber, Double longitude, Double latitude, String physicalAddress) {
-        this.id = id;
-        this.supplierId = supplierId;
-        this.name = name;
-        this.state = state;
-        this.currency = currency;
-        this.guestsNumber = guestsNumber;
-        this.longitude = longitude;
-        this.latitude = latitude;
-        this.physicalAddress = physicalAddress;
-    }
+	public Product(Integer id, Integer supplierId, String name, ProductState state, String currency, Integer guestsNumber, Double longitude, Double latitude, String physicalAddress) {
+		this.id = id;
+		this.supplierId = supplierId;
+		this.name = name;
+		this.state = state;
+		this.currency = currency;
+		this.guestsNumber = guestsNumber;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.physicalAddress = physicalAddress;
+	}
 
 }
 
