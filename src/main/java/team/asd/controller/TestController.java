bebe15419165e8daf6535a46957e0d88d944afa0
Roman Dovgan <romadovg@gmail.com>
2022-11-16 10:48:14 +0200

    Added test API to check MyBatis connection

--- src/main/java/team/asd/controller/TestController.java
+++ src/main/java/team/asd/controller/TestController.java
@@ -3,6 +3,7 @@ package team.asd.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import team.asd.entity.Message;
@@ -13,8 +14,10 @@ import java.time.LocalDate;
 @RestController
 public class TestController {
 	private final String message = "Test message from Spring Boot Application!";
+	private final TestMapper testMapper;
 
 	public TestController(@Autowired TestMapper testMapper) {
+		this.testMapper = testMapper;
 		//testMapper.insertValue("test1");
 	}
 
@@ -25,9 +28,18 @@ public class TestController {
 	}
 
 	@PostMapping("/test/message")
-	public Message message(@RequestBody String test_message) {
+	public Message message(@RequestBody String testMessage) {
 		try {
-			return new Message(LocalDate.now().plusDays(Integer.parseInt(test_message)), message);
+			return new Message(LocalDate.now().plusDays(Integer.parseInt(testMessage)), message);
+		} catch (NumberFormatException e) {
+			return new Message(LocalDate.now(), "Error - " + e.getMessage());
+		}
+	}
+	@PutMapping("/test/message")
+	public Message messageInsert(@RequestBody String testMessage) {
+		try {
+			testMapper.insertValue(testMessage);
+			return new Message(LocalDate.now(), "Message inserted");
 		} catch (NumberFormatException e) {
 			return new Message(LocalDate.now(), "Error - " + e.getMessage());
 		}
