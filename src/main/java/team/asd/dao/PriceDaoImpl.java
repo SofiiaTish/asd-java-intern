f5f04ed38da6e6dd926469aad1692190281a1f76
Sofiia Tishchenko <s.tischenko01@gmail.com>
2022-11-21 20:17:17 +0100

    #45 Add @Transactional to PRiceServiceImpl.

--- src/main/java/team/asd/dao/PriceDaoImpl.java
+++ src/main/java/team/asd/dao/PriceDaoImpl.java
@@ -2,6 +2,7 @@ package team.asd.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import team.asd.entity.Price;
 import team.asd.mapper.PriceMapper;
 
@@ -16,21 +17,25 @@ public class PriceDaoImpl implements PriceDao {
 	}
 
 	@Override
+	@Transactional
 	public Price readById(Integer id) {
 		return priceMapper.readPriceById(id);
 	}
 
 	@Override
+	@Transactional
 	public void savePrice(Price price) {
 		priceMapper.insertPrice(price);
 	}
 
 	@Override
+	@Transactional
 	public void updatePrice(Price price) {
 		priceMapper.updatePrice(price);
 	}
 
 	@Override
+	@Transactional
 	public void deletePrice(Integer id) {
 		priceMapper.deletePrice(id);
 	}
