60c262345886db51c639710152f7d97f5400d34f
Sofiia Tishchenko <s.tischenko01@gmail.com>
2022-11-21 19:51:31 +0100

    #45 *Mapper.xml -> refactor spaces on tabs in format. PriceMapper.xml -> add queries and result map. PriceDaoImpl -> add price mapper. PriceService -> refactor validation for create method.

--- src/main/java/team/asd/dao/PriceDaoImpl.java
+++ src/main/java/team/asd/dao/PriceDaoImpl.java
@@ -1,26 +1,37 @@
 package team.asd.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import team.asd.entity.Price;
+import team.asd.mapper.PriceMapper;
 
 
 @Repository
 public class PriceDaoImpl implements PriceDao {
 
+	private final PriceMapper priceMapper;
+
+	public PriceDaoImpl(@Autowired PriceMapper priceMapper) {
+		this.priceMapper = priceMapper;
+	}
+
 	@Override
 	public Price readById(Integer id) {
-		return null;
+		return priceMapper.readPriceById(id);
 	}
 
 	@Override
 	public void savePrice(Price price) {
+		priceMapper.insertPrice(price);
 	}
 
 	@Override
 	public void updatePrice(Price price) {
+		priceMapper.updatePrice(price);
 	}
 
 	@Override
 	public void deletePrice(Integer id) {
+		priceMapper.deletePrice(id);
 	}
 }
