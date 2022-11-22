9d47916da85bf72b4d7d368c354a51cfe3326970
Sofiia Tishchenko <s.tischenko01@gmail.com>
2022-11-23 00:15:20 +0100

    ConverterUtil -> refactor try-catch to use ?: instead.

--- src/main/java/team/asd/util/ConverterUtil.java
+++ src/main/java/team/asd/util/ConverterUtil.java
@@ -16,29 +16,31 @@ public class ConverterUtil {
 	private static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
 	public static Product convertDtoToProduct(ProductDto productDto) {
-		Product product = Product.builder().id(productDto.getId()).supplierId(productDto.getSupplierId()).name(productDto.getName())
-				.currency(productDto.getCurrency()).guestsNumber(productDto.getGuestsNumber()).longitude(productDto.getLongitude())
-				.latitude(productDto.getLatitude()).physicalAddress(productDto.getPhysicalAddress()).build();
-		try {
-			product.setState(ProductState.valueOf(productDto.getState()));
-		} catch (NullPointerException e3) {
-			product.setState(null);
-		} catch (IllegalArgumentException e1) {
-			throw new ValidationException("Invalid state of product");
-		}
-		return product;
+		return Product.builder()
+				.id(productDto.getId())
+				.supplierId(productDto.getSupplierId())
+				.name(productDto.getName())
+				.state(productDto.getState() == null ? null : ProductState.valueOf(productDto.getState()))
+				.currency(productDto.getCurrency())
+				.guestsNumber(productDto.getGuestsNumber())
+				.longitude(productDto.getLongitude())
+				.latitude(productDto.getLatitude())
+				.physicalAddress(productDto.getPhysicalAddress())
+				.build();
 	}
 
 	public static ProductDto convertProductToDto(Product product) {
-		ProductDto productDto = ProductDto.builder().id(product.getId()).supplierId(product.getSupplierId()).name(product.getName())
-				.currency(product.getCurrency()).guestsNumber(product.getGuestsNumber()).longitude(product.getLongitude()).latitude(product.getLatitude())
-				.physicalAddress(product.getPhysicalAddress()).build();
-		try {
-			productDto.setState(product.getState().toString());
-		} catch (NullPointerException e) {
-			productDto.setState(null);
-		}
-		return productDto;
+		return ProductDto.builder()
+				.id(product.getId())
+				.supplierId(product.getSupplierId())
+				.name(product.getName())
+				.state(product.getState() != null ? product.getState().toString() : null)
+				.currency(product.getCurrency())
+				.guestsNumber(product.getGuestsNumber())
+				.longitude(product.getLongitude())
+				.latitude(product.getLatitude())
+				.physicalAddress(product.getPhysicalAddress())
+				.build();
 	}
 
 	public static Price convertDtoToPrice(PriceDto priceDto) {
@@ -47,27 +49,17 @@ public class ConverterUtil {
 				.entityType(priceDto.getEntityType())
 				.entityId(priceDto.getEntityId())
 				.name(priceDto.getName())
+				.state(priceDto.getState() == null ? null : PriceState.valueOf(priceDto.getState()))
 				.value(priceDto.getValue())
 				.currency(priceDto.getCurrency())
 				.build();
 		try {
-			price.setState(PriceState.valueOf(priceDto.getState()));
-		} catch (NullPointerException e3) {
-			price.setState(null);
-		} catch (IllegalArgumentException e1) {
-			throw new ValidationException("Invalid state of price");
-		}
-		try {
-			price.setFromDate(format.parse(priceDto.getFromDate()));
-		} catch (NullPointerException e3) {
-			price.setFromDate(null);
-		} catch (ParseException e1) {
+			price.setFromDate(priceDto.getFromDate() == null ? null : format.parse(priceDto.getFromDate()));
+		} catch (ParseException e) {
 			throw new ValidationException("Invalid date of price");
 		}
 		try {
-			price.setToDate(format.parse(priceDto.getToDate()));
-		} catch (NullPointerException e3) {
-			price.setToDate(null);
+			price.setToDate(priceDto.getToDate() == null ? null : format.parse(priceDto.getToDate()));
 		} catch (ParseException e1) {
 			throw new ValidationException("Invalid date of price");
 		}
@@ -75,22 +67,17 @@ public class ConverterUtil {
 	}
 
 	public static PriceDto convertPriceToDto(Price price) {
-		PriceDto priceDto = PriceDto.builder()
+		return PriceDto.builder()
 				.id(price.getId())
 				.entityType(price.getEntityType())
 				.entityId(price.getEntityId())
 				.name(price.getName())
+				.state(price.getState() != null ? price.getState().toString() : null)
 				.value(price.getValue())
 				.fromDate(price.getFromDate() != null ? price.getFromDate().toString() : null)
 				.toDate(price.getToDate() != null ? price.getToDate().toString() : null)
 				.currency(price.getCurrency())
 				.build();
-		try {
-			priceDto.setState(price.getState().toString());
-		} catch (NullPointerException e) {
-			priceDto.setState(null);
-		}
-		return priceDto;
 	}
 
 	public static Fee convertDtoToFee(FeeDto feeDto) {
@@ -99,6 +86,7 @@ public class ConverterUtil {
 				.feeType(feeDto.getFeeType() == null ? null : FeeType.valueOf(feeDto.getFeeType()))
 				.productId(feeDto.getProductId())
 				.name(feeDto.getName())
+				.state(feeDto.getState() == null ? null : FeeState.valueOf(feeDto.getState()))
 				.taxType(feeDto.getTaxType() == null ? null : TaxType.valueOf(feeDto.getTaxType()))
 				.unit(feeDto.getUnit() == null ? null : Unit.valueOf(feeDto.getUnit()))
 				.value(feeDto.getValue())
@@ -106,23 +94,12 @@ public class ConverterUtil {
 				.currency(feeDto.getCurrency())
 				.build();
 		try {
-			fee.setState(FeeState.valueOf(feeDto.getState()));
-		} catch (NullPointerException e) {
-			fee.setState(null);
-		} catch (IllegalArgumentException e) {
-			throw new ValidationException("Invalid state of fee");
-		}
-		try {
-			fee.setFromDate(format.parse(feeDto.getFromDate()));
-		} catch (NullPointerException e) {
-			fee.setFromDate(null);
+			fee.setFromDate(feeDto.getFromDate() == null ? null : format.parse(feeDto.getFromDate()));
 		} catch (ParseException e) {
 			throw new ValidationException("Invalid from_date of fee");
 		}
 		try {
-			fee.setToDate(format.parse(feeDto.getToDate()));
-		} catch (NullPointerException e) {
-			fee.setToDate(null);
+			fee.setToDate(feeDto.getToDate() == null ? null : format.parse(feeDto.getToDate()));
 		} catch (ParseException e) {
 			throw new ValidationException("Invalid to_date of fee");
 		}
@@ -130,7 +107,7 @@ public class ConverterUtil {
 	}
 
 	public static FeeDto convertFeeToDto(Fee fee) {
-		FeeDto feeDto = FeeDto.builder()
+		return FeeDto.builder()
 				.id(fee.getId())
 				.feeType(fee.getFeeType() != null ? fee.getFeeType().toString() : null)
 				.productId(fee.getProductId())
@@ -144,6 +121,6 @@ public class ConverterUtil {
 				.valueType(fee.getValueType() != null ? fee.getValueType().toString() : null)
 				.currency(fee.getCurrency())
 				.build();
-		return feeDto;
+
 	}
 }
