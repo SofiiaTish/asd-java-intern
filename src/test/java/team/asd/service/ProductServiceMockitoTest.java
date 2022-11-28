832ead109532efaeba6e022350e82d87293659c2
Sofiia Tishchenko <s.tischenko01@gmail.com>
2022-11-28 11:16:25 +0100

    #57 Add mockito. Add tests for read and create. Problem with doAnswer.

---
+++ src/test/java/team/asd/service/ProductServiceMockitoTest.java
@@ -0,0 +1,62 @@
+package team.asd.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.reflect.Whitebox;
+import team.asd.constant.ProductState;
+import team.asd.dao.ProductDao;
+import team.asd.entity.Product;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@RunWith(MockitoJUnitRunner.class)
+class ProductServiceMockitoTest {
+
+	private ProductService productService;
+	@Mock
+	private ProductDao productDao;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+		productService = new ProductService(productDao);
+	}
+
+	@Test
+	void testReadById() {
+		Mockito.when(productDao.readById(1))
+				.thenReturn(Product.builder().id(1).name("Mock").build());
+		Product product = productService.readById(1);
+		assertNotNull(product);
+		assertEquals(1, product.getId());
+	}
+
+	@Test
+	void testCreateProduct() {
+		final Product[] product = new Product[1];
+		Mockito.doAnswer(invocation -> {
+			product[0] = Product.builder().id(1).build();
+			return null;
+		}).when(productDao).saveProduct(Mockito.any(Product.class));
+		Mockito.when(productDao.readById(1)).thenReturn(product[0]);
+
+		assertNotNull(productService.createProduct(Product.builder()
+				.id(1).supplierId(1).name("Mock").state(ProductState.Created).currency("usd").build()));
+	}
+
+	@Test
+	void updateProduct() {
+	}
+
+	@Test
+	void deleteProduct() {
+	}
+}
