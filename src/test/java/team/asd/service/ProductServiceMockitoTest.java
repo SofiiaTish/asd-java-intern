a66cf555d8c83539168cf72403a2e3f96e53e8ab
Sofiia Tishchenko <s.tischenko01@gmail.com>
2022-11-28 21:26:53 +0100

    #57 Update ProductServiceMockitoTest to test all methods.

--- src/test/java/team/asd/service/ProductServiceMockitoTest.java
+++ src/test/java/team/asd/service/ProductServiceMockitoTest.java
@@ -1,26 +1,23 @@
 package team.asd.service;
 
-import org.junit.jupiter.api.Assertions;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.powermock.reflect.Whitebox;
 import team.asd.constant.ProductState;
 import team.asd.dao.ProductDao;
 import team.asd.entity.Product;
+import team.asd.exception.ValidationException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @RunWith(MockitoJUnitRunner.class)
 class ProductServiceMockitoTest {
-
 	private ProductService productService;
 	@Mock
 	private ProductDao productDao;
@@ -43,15 +40,21 @@ class ProductServiceMockitoTest {
 		assertEquals(1, product.getId());
 	}
 
+	@SneakyThrows
 	@Test
 	void testCreateProduct() {
+		Exception e = assertThrows(ValidationException.class, () -> Whitebox.invokeMethod(productService, "validProduct", product, false));
+		assertEquals("Product is null", e.getMessage());
+
+		product = Product.builder().supplierId(2).build();
+		e = assertThrows(ValidationException.class, () -> Whitebox.invokeMethod(productService, "validProduct", product, false));
+		assertEquals("Required field is null", e.getMessage());
+
 		Mockito.doAnswer(invocation -> {
 			product = Product.builder().id(1).build();
 			return null;
 		}).when(productDao).saveProduct(Mockito.any(Product.class));
 		Mockito.when(productDao.readById(1)).thenAnswer(invocation -> product);
-
-		assertNull(productService.readById(1));
 
 		productService.createProduct(Product.builder()
 				.id(1).supplierId(1).name("Mock").state(ProductState.Created).currency("usd").build());
@@ -63,10 +66,29 @@ class ProductServiceMockitoTest {
 	}
 
 	@Test
-	void updateProduct() {
+	void testUpdateProduct() {
+		Mockito.doAnswer(invocation -> {
+			product = Product.builder().id(1).build();
+			return null;
+		}).when(productDao).updateProduct(Mockito.any(Product.class));
+		Mockito.when(productDao.readById(1)).thenAnswer(invocation -> product);
+
+		productService.updateProduct(Product.builder()
+				.id(1).supplierId(1).name("Mock").state(ProductState.Created).currency("usd").build());
+
+		assertNotNull(productService.readById(1));
+
+		Mockito.verify(productDao).updateProduct(Mockito.any());
+		Mockito.verify(productDao, Mockito.times(2)).readById(Mockito.anyInt());
+
+		product.setId(null);
+		Exception e = assertThrows(ValidationException.class, () -> Whitebox.invokeMethod(productService, "validProduct", product, true));
+		assertEquals("Incorrect Id: null or not positive", e.getMessage());
 	}
 
 	@Test
-	void deleteProduct() {
+	void testDeleteProduct() {
+		productService.deleteProduct(1);
+		Mockito.verify(productDao).deleteProduct(Mockito.anyInt());
 	}
 }
