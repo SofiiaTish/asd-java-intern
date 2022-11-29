d0b7a64da7c2e7e6d109a141826d34e6b2d5ce3b
Sofiia Tishchenko <s.tischenko01@gmail.com>
2022-11-29 12:35:26 +0100

    #57 ProductServiceTest -> add mock tests for all methods. PriceServiceTest -> add mock tests for all methods. FeeServiceTest -> add mock tests for all methods.

--- src/test/java/team/asd/service/FeeServiceTest.java
+++ src/test/java/team/asd/service/FeeServiceTest.java
@@ -1,8 +1,17 @@
 package team.asd.service;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.reflect.Whitebox;
+import team.asd.constant.*;
+import team.asd.dao.FeeDao;
 import team.asd.dao.FeeDaoTestImpl;
 import team.asd.data.FeeDataTest;
 import team.asd.entity.Fee;
@@ -11,13 +20,22 @@ import team.asd.exception.ValidationException;
 import java.time.Instant;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@RunWith(MockitoJUnitRunner.class)
 class FeeServiceTest {
-
 	private static FeeService feeService;
+	private FeeService mockFeeService;
+
+	@Mock
+	private FeeDao mockFeeDao;
 
 	private static Fee fee;
+	private static Fee mockFee;
+	private AutoCloseable mockClosable;
 
 	@BeforeAll
 	public static void setUpClass() {
@@ -27,6 +45,18 @@ class FeeServiceTest {
 	@BeforeEach
 	void setUp() {
 		fee = FeeDataTest.getExpectedFee();
+		mockFee = null;
+		mockClosable = MockitoAnnotations.openMocks(this);
+		mockFeeService = new FeeService(mockFeeDao);
+	}
+
+	@AfterEach
+	void tearDown() {
+		try {
+			mockClosable.close();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Test
@@ -34,6 +64,12 @@ class FeeServiceTest {
 		assertEquals(fee.getId(), feeService.readById(1).getId());
 		assertThrows(ValidationException.class, () -> feeService.readById(-1));
 		assertThrows(ValidationException.class, () -> feeService.readById(null));
+
+		Mockito.when(mockFeeDao.readById(1))
+				.thenReturn(Fee.builder().id(1).build());
+		Fee fee = mockFeeService.readById(1);
+		assertNotNull(fee);
+		assertEquals(1, fee.getId());
 	}
 
 	@Test
@@ -47,6 +83,35 @@ class FeeServiceTest {
 		fee.setToDate(Date.from(Instant.now()));
 		fee.setFromDate(Date.from(Instant.parse("2022-12-15T10:15:30Z")));
 		assertThrows(ValidationException.class, () -> feeService.createFee(fee));
+
+		Exception e = assertThrows(ValidationException.class, () -> Whitebox.invokeMethod(mockFeeService, "validFee", mockFee, false));
+		assertEquals("Fee is null", e.getMessage());
+
+		Mockito.doAnswer(invocation -> {
+			mockFee = Fee.builder().id(1).build();
+			return null;
+		}).when(mockFeeDao).saveFee(Mockito.any(Fee.class));
+		Mockito.when(mockFeeDao.readById(1)).thenAnswer(invocation -> mockFee);
+
+		assertNull(mockFeeService.readById(1));
+		mockFeeService.createFee(Fee.builder()
+				.id(1)
+				.feeType(FeeType.General)
+				.productId(2)
+				.name("Test")
+				.state(FeeState.Initial)
+				.fromDate(Date.from(Instant.parse("2022-12-10T10:15:30Z")))
+				.toDate(Date.from(Instant.parse("2022-12-15T10:15:30Z")))
+				.taxType(TaxType.Taxable)
+				.unit(Unit.Per_Day)
+				.value(34.56)
+				.valueType(ValueType.Flat)
+				.currency("usd")
+				.build());
+		assertNotNull(mockFeeService.readById(1));
+
+		Mockito.verify(mockFeeDao).saveFee(Mockito.any());
+		Mockito.verify(mockFeeDao, Mockito.atLeast(2)).readById(Mockito.anyInt());
 	}
 
 	@Test
@@ -56,6 +121,19 @@ class FeeServiceTest {
 		assertThrows(ValidationException.class, () -> feeService.updateFee(new Fee()));
 		fee.setId(null);
 		assertThrows(ValidationException.class, () -> feeService.updateFee(fee));
+
+		Mockito.doAnswer(invocation -> {
+			mockFee = Fee.builder().id(1).build();
+			return null;
+		}).when(mockFeeDao).updateFee(Mockito.any(Fee.class));
+		Mockito.when(mockFeeDao.readById(1)).thenAnswer(invocation -> mockFee);
+
+		assertNull(mockFeeService.readById(1));
+		mockFeeService.updateFee(Fee.builder().id(1).build());
+		assertNotNull(mockFeeService.readById(1));
+
+		Mockito.verify(mockFeeDao).updateFee(Mockito.any());
+		Mockito.verify(mockFeeDao, Mockito.atLeast(2)).readById(Mockito.anyInt());
 	}
 
 	@Test
@@ -83,5 +161,8 @@ class FeeServiceTest {
 	void testDeleteFee() {
 		assertThrows(ValidationException.class, () -> feeService.deleteFee(-1));
 		assertThrows(ValidationException.class, () -> feeService.deleteFee(null));
+
+		mockFeeService.deleteFee(1);
+		Mockito.verify(mockFeeDao).deleteFee(Mockito.anyInt());
 	}
 }
--- src/test/java/team/asd/service/PriceServiceTest.java
+++ src/test/java/team/asd/service/PriceServiceTest.java
@@ -1,9 +1,17 @@
 package team.asd.service;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.reflect.Whitebox;
 import team.asd.constant.PriceState;
+import team.asd.dao.PriceDao;
 import team.asd.dao.PriceDaoTestImpl;
 import team.asd.data.PriceDataTest;
 import team.asd.entity.Price;
@@ -12,13 +20,21 @@ import team.asd.exception.ValidationException;
 import java.time.Instant;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@RunWith(MockitoJUnitRunner.class)
 class PriceServiceTest {
-
 	private static PriceService priceService;
+	private PriceService mockPriceService;
 
+	@Mock
+	private PriceDao mockPriceDao;
 	private static Price price;
+	private static Price mockPrice;
+	private AutoCloseable mockClosable;
 
 	@BeforeAll
 	public static void setUpClass() {
@@ -38,6 +54,19 @@ class PriceServiceTest {
 				.value(34.56)
 				.currency("usd")
 				.build();
+
+		mockPrice = null;
+		mockClosable = MockitoAnnotations.openMocks(this);
+		mockPriceService = new PriceService(mockPriceDao);
+	}
+
+	@AfterEach
+	void tearDown() {
+		try {
+			mockClosable.close();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Test
@@ -45,6 +74,12 @@ class PriceServiceTest {
 		assertEquals(PriceDataTest.getExpectedPrice(), priceService.readById(1));
 		assertThrows(ValidationException.class, () -> priceService.readById(-1));
 		assertThrows(ValidationException.class, () -> priceService.readById(null));
+
+		Mockito.when(mockPriceDao.readById(1))
+				.thenReturn(Price.builder().id(1).build());
+		Price price = mockPriceService.readById(1);
+		assertNotNull(price);
+		assertEquals(1, price.getId());
 	}
 
 	@Test
@@ -53,6 +88,32 @@ class PriceServiceTest {
 		assertThrows(ValidationException.class, () -> priceService.createPrice(new Price()));
 		price.setName(null);
 		assertThrows(ValidationException.class, () -> priceService.createPrice(price));
+
+		Exception e = assertThrows(ValidationException.class, () -> Whitebox.invokeMethod(mockPriceService, "validPrice", mockPrice, false));
+		assertEquals("Price is null", e.getMessage());
+
+		Mockito.doAnswer(invocation -> {
+			mockPrice = Price.builder().id(1).build();
+			return null;
+		}).when(mockPriceDao).savePrice(Mockito.any(Price.class));
+		Mockito.when(mockPriceDao.readById(1)).thenAnswer(invocation -> mockPrice);
+
+		assertNull(mockPriceService.readById(1));
+		mockPriceService.createPrice(Price.builder()
+				.id(1)
+				.entityType("Product")
+				.entityId(4)
+				.name("Test")
+				.state(PriceState.Initial)
+				.fromDate(Date.from(Instant.parse("2022-12-10T10:15:30Z")))
+				.toDate(Date.from(Instant.parse("2022-12-15T10:15:30Z")))
+				.value(34.56)
+				.currency("usd")
+				.build());
+		assertNotNull(mockPriceService.readById(1));
+
+		Mockito.verify(mockPriceDao).savePrice(Mockito.any());
+		Mockito.verify(mockPriceDao, Mockito.atLeast(2)).readById(Mockito.anyInt());
 	}
 
 	@Test
@@ -62,11 +123,31 @@ class PriceServiceTest {
 		price.setId(null);
 		price.setCurrency("hrn");
 		assertThrows(ValidationException.class, () -> priceService.updatePrice(price));
+
+		Mockito.doAnswer(invocation -> {
+			mockPrice = Price.builder().id(1).fromDate(Date.from(Instant.parse("2022-10-15T10:15:30Z"))).build();
+			return null;
+		}).when(mockPriceDao).updatePrice(Mockito.any(Price.class));
+		Mockito.when(mockPriceDao.readById(1)).thenAnswer(invocation -> mockPrice);
+
+		assertNull(mockPriceService.readById(1));
+		mockPriceService.updatePrice(Price.builder()
+				.id(1).name("Mock").currency("usd").build());
+		assertNotNull(mockPriceService.readById(1));
+
+		Mockito.verify(mockPriceDao).updatePrice(Mockito.any());
+		Mockito.verify(mockPriceDao, Mockito.atLeast(2)).readById(Mockito.anyInt());
+
+		Exception e = assertThrows(ValidationException.class, () -> Whitebox.invokeMethod(mockPriceService, "validPrice", mockPrice, true));
+		assertEquals("From_date can not be earlier then current", e.getMessage());
 	}
 
 	@Test
 	void testDeletePrice() {
 		assertThrows(ValidationException.class, () -> priceService.deletePrice(-1));
 		assertThrows(ValidationException.class, () -> priceService.deletePrice(null));
+
+		mockPriceService.deletePrice(1);
+		Mockito.verify(mockPriceDao).deletePrice(Mockito.anyInt());
 	}
 }
