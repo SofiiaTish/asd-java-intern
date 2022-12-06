1038cbf65af7dabfb10164429fa130091c93f64c
Sofiia Tishchenko <s.tischenko01@gmail.com>
2022-12-06 10:22:15 +0100

    #61 Add general model description.

--- src/main/java/team/asd/config/SwaggerConfig.java
+++ src/main/java/team/asd/config/SwaggerConfig.java
@@ -6,12 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.Collections;
 
 @Configuration
 @ComponentScan({"team.asd"})
@@ -23,17 +19,6 @@ public class SwaggerConfig {
 				.apis(RequestHandlerSelectors.withClassAnnotation(Validated.class))
 				.paths(PathSelectors.any())
 				.build()
-				.apiInfo(apiInfo())
 				.useDefaultResponseMessages(false);
-	}
-
-	private ApiInfo apiInfo() {
-		return new ApiInfo(
-				"My REST API",
-				"Some custom description of API.",
-				"API TOS",
-				"Terms of service",
-				new Contact("John Doe", "www.example.com", "myeaddress@company.com"),
-				"License of API", "API license URL", Collections.emptyList());
 	}
 }
--- src/main/java/team/asd/dto/ProductDto.java
+++ src/main/java/team/asd/dto/ProductDto.java
@@ -1,6 +1,7 @@
 package team.asd.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,6 +15,7 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@ApiModel(value = "ProductModel", description = "Model consists of fields that match to table. Model is used for sending data to ProductApi.")
 public class ProductDto {
 
 	@ApiModelProperty(value = "The id of product", example = "4")
